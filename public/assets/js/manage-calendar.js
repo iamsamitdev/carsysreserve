50b40322905ddd1c3e2cfc6f9b6c911835b9ede1
Samit Koyom <samitkoyom@gmail.com>
2018-06-21 02:42:52 +0700

    load json to calendar

--- public/assets/js/manage-calendar.js
+++ public/assets/js/manage-calendar.js
@@ -1,10 +1,13 @@
 $(function(){
 
+    var baseAPIURL = "http://car-reserve.site/api/";
+    
+    /*
     var defaultEvents = [
         {
             title: 'Leanring Laravel',
-            start:'2018-06-20',
-            end: '2018-06-25',
+            start:'2018-06-19 00:00:00',
+            end: '2018-06-23 23:00:00',
             className: 'bg-info'
         },
         {
@@ -14,17 +17,35 @@ $(function(){
             className: 'bg-success'
         }
     ];
+    */
 
-    $("#calendar").fullCalendar({
-        defaultView: 'month',
-        slotDuration: '00:30:00',
-        minTime: '08:00:00',
-        maxTime:'19:00:00',
-        header:{
-            left:'prev,next  today',
-            center:'title',
-            right: 'month,agendaWeek,agendaDay'
-        },
-        events: defaultEvents
+    //defaultEvents = [{"title":"aaa","start":"2018-06-10 00:00:00","end":"2018-06-15 23:00:00"},{"title":"Seminar at Phuket","start":"2018-06-28 14:04:26","end":"2018-06-29 14:04:36"}];
+
+    // Get event from API
+   $.getJSON( baseAPIURL+"events", function(result) {
+        $("#calendar").fullCalendar({
+            defaultView: 'month',
+            slotDuration: '00:30:00',
+            minTime: '08:00:00',
+            maxTime:'19:00:00',
+            header:{
+                left:'prev,next  today',
+                center:'title',
+                right: 'month,agendaWeek,agendaDay'
+            },
+
+            events:result.data,
+            eventClick: function(ccalEvent, jsEvent, view) {
+                $("#successModal").modal("show");
+                $(".modal-title").text(ccalEvent.title);
+                $(".modal-body h3").text('Resource ID: '+ccalEvent.id);
+                $(".modal-body p").text('Description: '+ccalEvent.description);
+            },
+
+            displayEventTime: false,
+            selectable: true,
+            editable: false,
+        
+        });
     });
 });
--- resources/views/backend/pages/dashboard.blade.php
+++ resources/views/backend/pages/dashboard.blade.php
@@ -92,5 +92,26 @@
         <!-- ============================================================== -->
         <!-- End PAge Content -->
         <!-- ============================================================== -->
+
+        <!-- Modal -->
+        <div class="modal fade" id="successModal" tabindex="-1" role="dialog" aria-hidden="true" data-backdrop="static">
+        <div class="modal-dialog modal-dialog-centered" role="document">
+            <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalCenterTitle">Modal title</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <h3></h3>
+                <p></p>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+            </div>
+            </div>
+        </div>
+        </div>
         
 @endsection
